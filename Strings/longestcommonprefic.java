d63e22c42101f32442b9dfa787d67c7b6d82d14d
sidharthsharma144 <sidharthsharma17240@gmail.com>
2024-09-10 23:55:39 +0530

    map sorted algorithm

---
+++ Strings/longestcommonprefic.java
@@ -0,0 +1,12 @@
+package Strings;
+import java.util.Scanner;
+public class longestcommonprefic {
+    public static void main(String[] args)
+    {
+        Scanner sc= new Scanner(System.in);
+        String str1=sc.nextLine();
+        String str2=sc.nextLine();
+        for (int i=)
+    }
+    
+}

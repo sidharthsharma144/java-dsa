715dc1de2e39f2c624045a79d83662e28a95ddc7
sidharthsharma144 <sidharthsharma17240@gmail.com>
2024-09-28 20:05:44 +0530

    question solved

--- exception handling/intro.java
+++ exception handling/intro.java
@@ -3,5 +3,6 @@ public class intro {
     public static void main(String[] args){
         Scanner sc=new Scanner(System.in);
         String name = sc.nextLine();
+        System.out.println(name);
     }
 }

e3c898c271a6324f75735851275917a6274d3a01
sidharthsharma144 <sidharthsharma17240@gmail.com>
2024-10-02 20:27:00 +0530

    question solved

--- array/checksortedarray.java
+++ array/checksortedarray.java
@@ -4,6 +4,6 @@ public class checksortedarray {
     public static void main(String[] args){
         Scanner sc=new Scanner(System.in);
         int size=sc.nextInt();
-        int[] arr=new int[size];;;;;;;
+        int[] arr=new int[size];;;;;;
     }
 }

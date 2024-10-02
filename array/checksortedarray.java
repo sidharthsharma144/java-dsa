388495445beee285527f07f6f917bf99c968a812
sidharthsharma144 <sidharthsharma17240@gmail.com>
2024-10-02 20:42:21 +0530

    question solved

--- array/checksortedarray.java
+++ array/checksortedarray.java
@@ -3,7 +3,23 @@ import java.util.Scanner;
 public class checksortedarray {
     public static void main(String[] args){
         Scanner sc=new Scanner(System.in);
-        int size=sc.nextInt();
-        int[] arr=new int[size];
+        int[] arr={1,2,4};
+        
+        boolean issorted=false;
+        for(int i=0;i<arr.length-1;i++){
+            if(arr[i]<arr[i+1]){
+                issorted=true;
+                
+            }
+            else{
+                issorted=false;
+            }
+        
+        }if(issorted){
+            System.out.println("yes sorted");
+        }
+        else{
+            System.out.println("not sorted");
+        }
     }
 }

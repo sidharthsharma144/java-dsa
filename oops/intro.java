2e4719f2eb29029e3447132de674d993f905dd0d
sidharthsharma144 <sidharthsharma17240@gmail.com>
2024-10-05 15:47:02 +0530

    question solved

--- oops/intro.java
+++ oops/intro.java
@@ -1,12 +1,14 @@
+//OBJECTS ARE INSTANCE OF CLASS
 public class intro{
     public static class Student{
     
     public String name;
-    public int roll;
     public double percentage;
+    public int roll;
     }
     public static void main(String[] args) {
-        Student a=new Student();
+        
+        Student a=new Student();   // this is for declaration of dbba or u can say we created the objects
         a.name="sidharth";
         a.roll=79;
         a.percentage=74;

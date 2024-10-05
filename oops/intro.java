e4ab8b3778d501dc0a8adcf15c2280eaa3573f9d
sidharthsharma144 <sidharthsharma17240@gmail.com>
2024-10-05 12:03:19 +0530

    question solved

---
+++ oops/intro.java
@@ -0,0 +1,15 @@
+public class intro{
+    public static class Student{
+    
+    public String name;
+    public int roll;
+    public double percentage;
+    }
+    public static void main(String[] args) {
+        Student a=new Student();
+        a.name="sidharth";
+        a.roll=79;
+        a.percentage=74;
+        System.out.println(a.roll); 
+    }
+}

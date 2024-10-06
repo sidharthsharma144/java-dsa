c97d505e770b552ba66a7038cb70460d00087042
sidharthsharma144 <sidharthsharma17240@gmail.com>
2024-10-06 18:26:29 +0530

    question solved

--- oops/functionchange.java
+++ oops/functionchange.java
@@ -1,27 +1,32 @@
 // CHANGING THE VALUE IN THE FUNCTION OF CLASS
+
+import java.util.Scanner;
+
 public class functionchange {
 
     public static class Cars{
-        private  String name;
+        public  String name;
         public String model;
         public double speed;
         public double price;
     }
 
-    // public static void update(Cars a){
-    //     a.name="mercedes";
+    public static void update(Cars a){
+        a.name="mercedes";
    
-    // }
+    }
     public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        
         Cars s=new Cars();
 
-        s.name="porsche";
+        s.name=sc.nextLine();
         s.model="B3";
         s.speed=394.4;
         
         
         System.out.println(s.name);
-        // update(s);
+        update(s);
         System.out.println(s.name);
         System.out.println(s.price);;
 

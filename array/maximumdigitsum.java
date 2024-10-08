d18819266b418df870221d31ec8f7e1330d609ff
sidharthsharma144 <sidharthsharma17240@gmail.com>
2024-10-08 21:04:28 +0530

    question solved

---
+++ array/maximumdigitsum.java
@@ -0,0 +1,26 @@
+import java.util.*;
+public class maximumdigitsum {
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        int size=sc.nextInt();
+        int[] arr=new int[size];
+        for(int i=0;i<size;i++){
+            arr[i]=sc.nextInt();
+        }
+        int n=0;
+        for(int i=0;i<size;i++){
+            int number=arr[i];
+            while(number!=0){
+            n=n+number%10;
+            number=number/10;
+        }
+    }
+        Arrays.sort(arr);
+
+        int [] a=new int[size];
+        int [] b=new int [size];
+        for(int i=0;i<size;i++){
+            a[i]=arr[size-i];
+        }
+    }
+}

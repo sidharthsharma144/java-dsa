aa5d1bc2030265bdf19b43bc132756d363d8abf1
sidharthsharma144 <sidharthsharma17240@gmail.com>
2025-01-03 12:31:19 +0530

    question solved

--- DP/fibonacci.java
+++ DP/fibonacci.java
@@ -1,16 +1,16 @@
 import java.util.*;
 public class fibonacci {
-    public static int fiboseries(int number){
+    public static int fiboseries(int n){
         if(n==1||n==0){
             return n;
         }
-        
+        return 0;
     }
     public static void main(String[] args){
         Scanner sc=new Scanner(System.in);
         int number=sc.nextInt();
         fiboseries(number);
-        sc.close();
+        sc.close();;;;;;;;;;;;;;;
     }
     
 }

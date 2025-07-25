64b9165dd34bd2b60bae228ddf7c5d6758a51761
sidharthsharma144 <sidharthsharma17240@gmail.com>
2025-07-25 23:43:24 +0530

    error solved

--- oops/intro/RotateArray.java
+++ oops/intro/RotateArray.java
@@ -54,6 +54,6 @@ public class RotateArray {
     public static void main(String[] args) {
         int[] arr = {1, 2, 3, 4, 5, 6, 7};
         rotate(arr, 3);
-        for (int num : arr) System.out.print(num + " ");;;;;
+        for (int num : arr) System.out.print(num + " ");;;;
     }
 }
